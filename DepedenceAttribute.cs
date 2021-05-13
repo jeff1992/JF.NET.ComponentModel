fed7cd6f0095307889d3922f5defe3fa9ea41d19
jeff1992 <327455748@qq.com>
2021-05-13 10:30:28 +0800

    添加项目文件。

---
+++ DepedenceAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JF.NET.ComponentModel
+{
+    public class DepedenceAttribute
+    {
+        public DepedenceAttribute(string propertyPath)
+        {
+            this.PropertyPath = propertyPath;
+        }
+        public string PropertyPath { get; private set; }
+    }
+}
